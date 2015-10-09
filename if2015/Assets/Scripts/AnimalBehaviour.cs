fd4b929ccbf0e08d89b869baa812fef192a09ef1
Lorenzo Iovino <thisloke@gmail.com>
2015-10-09 20:03:34 +0200

    Aggiunta scena di gioco principale

---
+++ if2015/Assets/Scripts/AnimalBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalBehaviour : MonoBehaviour {
+
+	public GameObject animals;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			animals.transform.position = new Vector3(animals.transform.position.x, animals.transform.position.y,animals.transform.position.z - 0.2f);
+
+		}
+		if (Input.GetMouseButton (1)) {
+			animals.transform.position = new Vector3(animals.transform.position.x, animals.transform.position.y,animals.transform.position.z + 0.2f);
+			
+		}
+	
+	}
+}
